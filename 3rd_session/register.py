701d3b95aa63c6ddf182b34cfdf41891ddb5c0fd
SEOMUNJI <tjanswl@naver.com>
2024-03-26 19:42:52 +0900

    #3 회원정보 딕셔너리 저장

--- 3rd_session/register.py
+++ 3rd_session/register.py
@@ -1,7 +1,7 @@
 #1
-print('=================================')
+print('===================================')
 print('회원가입')
-print('=================================')
+print('===================================')
 
 while True:
     print('회원가입을 진행하시겠습니까?')
@@ -9,14 +9,14 @@ while True:
     answer=input('>> ')
     answer=answer.lower()
     if answer=='y':
-        print('=================================')
+        print('===================================')
         print('회원가입이 진행됩니다.')
-        print('=================================')
+        print('===================================')
         break
     elif answer=='n':
-        print('=================================')
+        print('===================================')
         print('회원가입이 취소됩니다.')
-        print('=================================')
+        print('===================================')
         exit()
     else:
         print("'y' 또는 'n'를 입력해주세요.")
@@ -52,4 +52,29 @@ while True:
 
     #이메일 입력
     email=input('이메일: ')
-    
+
+#3
+    user['username']=username
+    user['password']=pwd
+    user['name']=name
+    user['birth']=birth
+    user['email']=email
+
+    users.append(user)
+    print(users)
+
+    print('===================================')
+    print(user['name'], '님, 가입을 환영합니다!')
+    print('===================================')
+    print('회원가입을 추가로 진행하시겠습니까?')
+    print('y: 진행                    n: 취소')
+    answer=input('>> ')
+    answer=answer.lower()
+
+    if answer=='y':
+        pass
+    elif answer=='n':
+        exit()
+    else:
+        exit()
+
