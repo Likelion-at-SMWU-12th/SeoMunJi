29fae63107c8a55a6d9d989d19b426c78983005f
SEOMUNJI <tjanswl@naver.com>
2024-07-16 00:59:44 +0900

    7-10주차 재실습

---
+++ week07/posts/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from rest_framework.viewsets import ModelViewSet
+
+from django.views.generic import ListView
+from .models import Post
+from .forms import PostBasedForm, PostModelForm
+from .serializers import PostModelSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def calculator(request):
+    data = request.data
+    num1=data.get('num1', 0)
+    num2=data.get('num2', 0)
+    operator=data.get('operator')
+
+    if operator == '+':
+        result = int(num1) + int(num2)
+    elif operator == '-':
+        result = int(num1) - int(num2)
+    elif operator == '*':
+        result = int(num1) * int(num2)
+    elif operator == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+
+    data = {
+        'operator' : operator,
+        'result' : result
+    }
+    return Response(data)
+
+class PostModelViewSet(ModelViewSet):
+    queryset=Post.objects.all()
+    serializer_class=PostModelSerializer
+
+def index(request):
+    return render(request, 'index.html')
+
+def post_list_view(request):
+    return render(request, 'posts/post_list.html')
+
+def post_detail_view(request, id):
+    return render(request, 'posts/post_detail.html')
+
+def post_create_view(request):
+    return render(request, 'posts/post_form.html')
+
+def post_form_view(request):
+    if request.method == "GET":
+        form = PostBasedForm()
+        context = {'form': form}
+        return render(request, 'posts/post_form2.html', context)
+    else:
+        return redirect('index')
+    
+def post_create_form_view(request):
+    if request.method == "GET":
+        form = PostModelForm()
+        context = {'form': form}
+        return render(request, 'posts/post_form2.html', context)
+    else:
+        form = PostModelForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            Post.objects.create(
+                image = form.cleaned_data['image'],
+                content = form.cleaned_data['content'],
+                writer = request.user
+            )
+        else:
+            return redirect('post:post-new')
+        return redirect('index')
+    
+def post_update_view(request, id):
+    return render(request, 'posts/post_update.html')
+
+def post_delete_view(request, id):
+    return render(request, 'posts/post_confirm_delete.html')
+
+class class_view(ListView):
+    model = Post
+    template_name='cbv_view.html'
+
+def url_view(request):
+    data={'code':'001','msg':'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET:{request.GET}')
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method:{request.method}')
+    if request.method =="GET":
+        print(f'request.GET:{request.GET}')
+    elif request.method == "POST":
+        print(f'request.POST:{request.POST}')
+    return render(request,'view.html')
