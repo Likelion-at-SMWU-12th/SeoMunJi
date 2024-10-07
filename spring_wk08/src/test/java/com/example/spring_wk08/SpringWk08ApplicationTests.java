4219761f2f6d3bb2cbdc2312e94249ec36a2847e
SEOMUNJI <tjanswl@naver.com>
2024-10-07 17:30:34 +0900

    데이터베이스 생성

---
+++ spring_wk08/src/test/java/com/example/spring_wk08/SpringWk08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_wk08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWk08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
